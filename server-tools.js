73c9436a6aa8b32f9449b7430e567850618c075d
Casey Grun <casey.grun@wyss.harvard.edu>
2011-07-25 13:24:44 -0700

    Improved documentation

--- server-tools.js
+++ server-tools.js
@@ -1,20 +1,59 @@
-var utils = require('./utils'), auth = require('./auth'), proc = require('child_process'), fs = require('fs'), _ = require('underscore'), async = require('async'), path = require('path'), DNA = require('./static/lib/dna-utils').DNA;
+var utils = require('./utils'), auth = require('./auth'), proc = require('child_process'), fs = require('fs'), _ = require('underscore'), async = require('async'), path = require('path'), DNA = require('./static/lib/dna-utils').DNA, validate = require('validator'), winston = require('winston');
+var check = validate.check, sanitize = validate.sanitize;
 
 var sendError = utils.sendError, forbidden = utils.forbidden, allowedPath = utils.allowedPath, getCommand = utils.getCommand;
 
+/*
+ * @class {Server.Tool}
+ * Encapsulates a functions necessary to perform a task on one of various targets.
+ */
 var toolsList = require('./tools-list').list, tools = _.reduce(toolsList, function(memo, name) {
 	memo[name] = require('./' + path.join('server', name));
 	_.defaults(memo[name], {
+		/**
+		 * @cfg {String} route
+		 * The path relative to {@link Server#baseRoute} at which this tool will be exposed as an HTTP resource
+		 */
 		route : name,
+		/**
+		 * @cfg {String[]} params
+		 * Array of parameters to be read and sanitized from req.params
+		 */
 		params : [],
+		/**
+		 * @cfg {Function} start
+		 * Function to be called to start the tool
+		 * @param {Function} callback
+		 */
 		start : function(cb) {
 		},
+		/**
+		 * @cfg {Function} interrupt
+		 * Function to be called to interrupt and terminate execution of the tool. Must kill any running processes locally or
+		 * on LSF. May clean up unused files.
+		 */
 		interrupt : function(err, cb) {
 		},
+		/**
+		 * @cfg {Function} after
+		 * Function to be called after the task completes
+		 */
 		after : function(err) {
 		},
 		command : '',
 		arguments : [],
+
+		/**
+		 * @cfg {String} name
+		 * Name of the tool
+		 * @client
+		 */
+
+		/**
+		 * @cfg {String} iconCls
+		 * CSS class of an icon to be used to represent to tool
+		 * @client
+		 */
 	});
 	return memo;
 }, {});
@@ -24,7 +63,7 @@ var toolsList = require('./tools-list').list, tools = _.reduce(toolsList, functi
  */
 exports.configure = function(app, express) {
 	var baseRoute = app.set('baseRoute');
-	
+
 	// Expose a handler for each tool
 	_.each(tools, function(spec, name) {
 		app.post(path.join(baseRoute, spec.route), auth.restrict('json'), function(spec, name) {
@@ -40,7 +79,6 @@ exports.configure = function(app, express) {
 			}
 		}(spec, name));
 	});
-	
 	// Expose a list of tools to be automatically defined as {@link App.TaskRunner.Tool} subclasses
 	app.get(path.join(baseRoute, '/toolslist'), auth.restrict('json'), function(req, res) {
 		var clientParams = ['name', 'iconCls', 'route'], clientTools = _.map(tools, function(block, name) {

6fc8c26a4a3ae05081f306b49a8a32a7bd87513d
Casey Grun <casey.grun@wyss.harvard.edu>
2011-07-27 14:23:14 -0700

    Added simple help application

---
+++ static/client/ui/Help.js
@@ -0,0 +1,10 @@
+Ext.define('App.ui.Help',{
+	extend: 'App.ui.Browser',
+	title: 'Help',
+	iconCls: 'help',
+	trigger: 'index',
+	constructor: function(config) {
+		this.url = App.path.join(['help',config.triggers[0] || 'index']);
+		this.callParent(arguments);
+	}
+})

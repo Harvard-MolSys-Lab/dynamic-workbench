Harvard-MolSys-Lab/dynamic-workbench

commit c3b2ca476a357e2c60c13f62899038e54a1eaa4e

Upgraded raphael to 2.0 and made some necessary changes to vector object handling. Added arrows. Regression encountered where rotating no longer works, so input nodes aren't rotated correctly now.

static/client/workspace/Proxy.js
@@ -14,7 +14,7 @@ Ext.define('Workspace.Proxy', {
 	 * @cfg {Boolean}
 	 * Force the proxy to push itself to the back whenever shown (useful for highlight proxies)
 	 */
-	forceBack: false,
+	forceBack : false,
 	/**
 	 * @cfg {String} shape
 	 * Name of a Raphael member function to use to create a vector element
@@ -56,6 +56,8 @@ Ext.define('Workspace.Proxy', {
 	 * @cfg {Number} y
 	 */
 	y : 0,
+	rx: 0,
+	ry: 0,
 	/**
 	 * @cfg {Number} width
 	 */
@@ -162,8 +164,8 @@ Ext.define('Workspace.Proxy', {
 	 */
 	getDelta : function(x2, y2) {
 		return {
-			dx : ( x2 -    this.getX()),
-			dy : ( y2 -    this.getY())
+			dx : (x2 - this.getX()),
+			dy : (y2 - this.getY())
 		};
 	},
 	getScaleFactor : function(w2, h2) {
@@ -183,7 +185,18 @@ Ext.define('Workspace.Proxy', {
 		this.x += dx;
 		this.y += dy;
 		if(dx != 0 || dy != 0) {
-			this.vectorElement.translate(dx, dy);
+			if(this.shape == 'path') {
+				var path = Raphael.pathToRelative(this.vectorObject.attr('path'));
+				path[0][1] += +dx;
+				path[0][2] += +dy;
+				this.updatePath(this.path);
+			} else {
+				this.vectorElement.attr({
+					x : this.x,
+					y : this.y,
+				});
+			}
+			//this.vectorElement.translate(dx, dy);
 		}
 		this.fireEvent('move', this.x, this.y);
 	},
@@ -248,7 +261,7 @@ Ext.define('Workspace.Proxy', {
 	setBox : function(x1, y1, x2, y2) {
 		if(arguments.length == 4) {
 			this.setPosition(x1, y1);
-			this.setDimensions( x2 - x1, y2 - y1);
+			this.setDimensions(x2 - x1, y2 - y1);
 		} else {
 			var box = arguments[0];
 			this.setDimensions(box.tr.x - box.tl.x, box.bl.y - box.tl.y);
@@ -296,7 +309,7 @@ Ext.define('Workspace.Proxy', {
 		this.setDimensions(w, h);
 	},
 	updateTransform : function(source) {
-		this.vectorElement.attr('rotation', source.get('rotation'));
+		this.vectorElement.attr('transform', "R" + (+source.get('rotation')));
 		// this.vectorElement.attr('scale',source.get('scale'));
 	},
 	/**
@@ -342,7 +355,7 @@ Ext.define('Workspace.Proxy', {
 	show : function() {
 		if(this.vectorElement) {
 			this.vectorElement.show();
-			if(this.forceBack) {				
+			if(this.forceBack) {
 				this.vectorElement.toBack();
 			}
 		}

static/client/workspace/objects/Path.js
@@ -116,26 +116,30 @@ Ext.define('Workspace.objects.Path', {
 			}
 			this.set('points',points);
 		} else {
-			var point;
-			for( i = 0, l = this.path.length; i < l; i++) {
-				point = this.path[i];
-				switch (point[0]) {
-					case 'C':
-						point[5] = point[5] + dx;
-						point[6] = point[6] + dy;
-					case 'S':
-						point[3] = point[3] + dx;
-						point[4] = point[4] + dy;
-					case 'M':
-					case 'L':
-					case 'T':
-						point[1] = point[1] + dx;
-						point[2] = point[2] + dy;
-						break;
-					default:
-						break;
-				}
-			}
+			// var point;
+			// for( i = 0, l = this.path.length; i < l; i++) {
+				// point = this.path[i];
+				// switch (point[0]) {
+					// case 'C':
+						// point[5] = point[5] + dx;
+						// point[6] = point[6] + dy;
+					// case 'S':
+						// point[3] = point[3] + dx;
+						// point[4] = point[4] + dy;
+					// case 'M':
+					// case 'L':
+					// case 'T':
+						// point[1] = point[1] + dx;
+						// point[2] = point[2] + dy;
+						// break;
+					// default:
+						// break;
+				// }
+			// }
+			this.path = Raphael.pathToRelative(this.path);
+	            this.path[0][1] = this.path[0][1]+dx;
+	            this.path[0][2] = this.path[0][2]+dy;
+			//this.set('path',this.path);
 			this.updatePath(this.path);
 		}
 		/*

static/client/workspace/objects/VectorObject.js
@@ -8,7 +8,9 @@
 Ext.define('Workspace.objects.VectorObject', {
 	alias : 'Workspace.VectorObject',
 	statics : {
-		attrArray : ['clip-rect', 'fill', 'fill-opacity', 'font', 'font-family', 'font-size', 'font-weight', 'height', 'opacity', 'path', 'r', 'rotation', 'rx', 'ry',    /*'scale',*/ 'src', 'stroke', 'stroke-dasharray', 'stroke-linecap', 'stroke-linejoin', 'stroke-miterlimit', 'stroke-opacity', 'stroke-width', 'translation', 'width', 'x', 'y'],
+		attrArray : ['arrow-end','arrow-start','clip-rect', 'fill', 'fill-opacity', 'font', 'font-family', 'font-size', 'font-weight', 'height', 'opacity', 'path', 'r', 
+'rotation', 
+		'rx', 'ry',    /*'scale',*/ 'src', 'stroke', 'stroke-dasharray', 'stroke-linecap', 'stroke-linejoin', 'stroke-miterlimit', 'stroke-opacity', 'stroke-width', 'translation', 'width', 'x', 'y'],
 	},
 	constructor : function(workspace, config) {
 		Workspace.objects.VectorObject.superclass.constructor.call(this, workspace, config);
@@ -39,6 +41,8 @@ Ext.define('Workspace.objects.VectorObject', {
 		this.expose('shape', true, true, true, false);
 		//,'shape','string');
 		this.expose('rotation', true, true, true, false);
+		this.expose('arrowEnd',true,true,true,false);
+		this.expose('arrowStart',true,true,true,false);
 
 	},
 	requires: ['Workspace.Proxy'],
@@ -88,7 +92,8 @@ Ext.define('Workspace.objects.VectorObject', {
 	 * Number from 0-1 indicating opacity of the stroke
 	 */
 	strokeOpacity : 1,
-
+	rotation: 0,
+	
 	render : function() {
 		this.buildObject();
 		// this.on('change', this.updateObject, this);
@@ -142,6 +147,10 @@ Ext.define('Workspace.objects.VectorObject', {
 		// hack to prevent anti-aliasing for rectangular things
 		if(attrName=='x' || attrName=='y') {
 			value = Math.round(value)+0.5;
+		} else if (attrName=="rotation") {
+			// this.vectorElement.transform("");
+			// this.vectorElement.rotate(value);
+			//this.vectorElement.transform("r"+ (+value || 0));
 		}
 		this.vectorElement.attr(attrName, value);
 	},

server/manifest.js
@@ -8,10 +8,11 @@ var stylePath = 'static/styles';
 var extStyles = extPath + '/resources/css/ext-all';
 var codeMirrorVersion = '2.13';
 var codeMirrorPath = libPath + '/CodeMirror-'+codeMirrorVersion;
+var raphaelVersion = '2.0.1';//'1.5.2';
 
 var styles = [extStyles, 'styles/canvas', 'styles/icons', 'styles/infomachine', 'styles/colaborate/stylesheet', libPath + '/color-field-1.0.0/color-field', codeMirrorPath + '/lib/codemirror', codeMirrorPath + '/theme/default', libPath + '/valums-file-uploader/client/fileuploader', libPath + '/ux/css/CheckHeader',
 libPath + '/extjs-boxselect/src/BoxSelect'];
-var libs = ['jquery-1.5.1.min', 'underscore', 'string', 'color-field-1.0.0/color-field', 'raphael/raphael-min', 'raphael/plugins/raphael.primitives', 'CodeMirror-'+codeMirrorVersion+'/lib/codemirror', 'CodeMirror-'+codeMirrorVersion+'/lib/runmode', 'valums-file-uploader/client/fileuploader', 'dna-utils', 'Ext.ux.StatusBar', 'protovis-3.2/protovis-d3.2','/extjs-boxselect/src/BoxSelect'];
+var libs = ['jquery-1.5.1.min', 'underscore', 'string', 'color-field-1.0.0/color-field', 'raphael-'+raphaelVersion+'/raphael-min', 'raphael-'+raphaelVersion+'//plugins/raphael.primitives', 'CodeMirror-'+codeMirrorVersion+'/lib/codemirror', 'CodeMirror-'+codeMirrorVersion+'/lib/runmode', 'valums-file-uploader/client/fileuploader', 'dna-utils', 'Ext.ux.StatusBar', 'protovis-3.2/protovis-d3.2','/extjs-boxselect/src/BoxSelect'];
 var uxs = ['RowExpander', 'CheckColumn'];
 var modes = ['javascript', 'stex', 'xml', 'diff', 'htmlmixed', 'css', 'clike','python','ruby','coffeescript',];
 

static/client/workspace/objects/Connection.js
@@ -136,6 +136,7 @@ Ext.define('Workspace.objects.Connection', {
 		this.arguments = [this.path];
 		Workspace.objects.Connection.superclass.render.apply(this, arguments);
 		this.updateDimensions();
+		//this.vectorElement.attr({"arrow-end":"classic-wide-long",});
 	},
 	getHighlightProxy : function() {
 		return new Workspace.Proxy(Ext.applyIf({

static/client/workspace/objects/dna/Complementarity.js
@@ -1,18 +1,30 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 Ext.define('Workspace.objects.dna.Complementarity', {
-	extend:'Workspace.objects.Connection',
-	showLabel: false,
-	property: 'complementarity',
-	acceptLeft: function(item) {
+	extend : 'Workspace.objects.Connection',
+	showLabel : false,
+	property : 'complementarity',
+	render : function() {
+		this.callParent();
+		// this.vectorElement.attr({
+			// "arrow-end" : "classic-wide-long",
+		// });
+		this.set('strokeWidth',1.5);
+		this.set('stroke',"#aaa");
+		this.set('arrowEnd',"classic-wide-long");
+	},
+	strokeWidth : 1.5,
+	arrowEnd:"classic-wide-long",
+	stroke:"#999",
+	acceptLeft : function(item) {
 		return (item.getId && (item.isWType('Workspace.objects.dna.OutputPort') || item.isWType('Workspace.objects.dna.BridgePort')));
 	},
-	acceptRight: function(item) {
-		return (item.getId && (item.isWType('Workspace.objects.dna.InputPort')|| item.isWType('Workspace.objects.dna.BridgePort')));
+	acceptRight : function(item) {
+		return (item.getId && (item.isWType('Workspace.objects.dna.InputPort') || item.isWType('Workspace.objects.dna.BridgePort')));
 	},
-	canConnect: function(left,right) {
+	canConnect : function(left, right) {
 		return (left.isWType('Workspace.objects.dna.OutputPort') && right.isWType('Workspace.objects.dna.InputPort')) || (left.isWType('Workspace.objects.dna.BridgePort') && right.isWType('Workspace.objects.dna.BridgePort'));
 	},
 }, function() {
-	Workspace.reg('Workspace.objects.dna.Complementarity',Workspace.objects.dna.Complementarity);
+	Workspace.reg('Workspace.objects.dna.Complementarity', Workspace.objects.dna.Complementarity);
 });

static/client/workspace/tools/ConnectorTool.js
@@ -15,12 +15,13 @@ Ext.define('Workspace.tools.ConnectorTool', {
 	 */
 	constructor: function(workspace, config) {
 		this.workspace = workspace;
-		Ext.apply(this, config, {
-			parameters: {}
-		});
+		Ext.applyIf(this, config);
 		Workspace.tools.ConnectorTool.superclass.constructor.call(this, workspace, config);
 
-		Ext.apply(this.parameters, App.Stylesheet.Draw);
+		if(!this.parameters) {
+			this.parameters = {};
+		}
+		Ext.applyIf(this.parameters, App.Stylesheet.Draw);
 
 		this.dragging = false;
 		this.proto = false;
@@ -97,6 +98,7 @@ Ext.define('Workspace.tools.ConnectorTool', {
 	        this.dragging = true;
 	        if (this.proto){
 	            this.proto.remove();
+	            delete this.proto;
 	        }
 			
 	        this.createProto(e, item);
@@ -137,6 +139,7 @@ Ext.define('Workspace.tools.ConnectorTool', {
 	            delete this.rightObject;
 	            
 	            this.proto.remove();
+	            delete this.proto;
 	        }
 	        e.stopEvent();
         }

static/client/workspace/tools/nodal/ComplementarityTool.js
@@ -3,6 +3,12 @@
 Ext.define('Workspace.tools.nodal.ComplementarityTool', {
 	extend:'Workspace.tools.ConnectorTool',
 	require: ['Workspace.objects.dna.Complementarity',],
+	parameters: {
+		"arrow-end": 'classic-wide-long',
+		"stroke-dasharray": '.',
+		"stroke":"#aaa",
+		"stroke-width":1.5,
+	},
 	acceptLeft: function() {
 		return Workspace.objects.dna.Complementarity.prototype.acceptLeft.apply(this,arguments);
 	},

commit 933b2912ad6477581ab467442788eed856ab3f2d

Improved nodal species rendering by restoring rotation support broken in c3b2ca476a357e2c60c13f62899038e54a1eaa4e

static/client/workspace/Proxy.js
@@ -117,7 +117,7 @@ Ext.define('Workspace.Proxy', {
 	 * @private
 	 */
 	updateDimensionProps : function() {
-		var box = this.vectorElement.getBBox();
+		var box = this.vectorElement.getBBox(true);
 		this.x = (box.x);
 		this.y = (box.y);
 		this.height = (box.height);

static/client/workspace/objects/Path.js
@@ -91,7 +91,7 @@ Ext.define('Workspace.objects.Path', {
 	 * @private
 	 */
 	updateDimensions : function() {
-		var box = this.vectorElement.getBBox();
+		var box = this.vectorElement.getBBox(true);
 		this.set('x', box.x);
 		this.set('y', box.y);
 		this.set('height', box.height);

static/client/workspace/objects/VectorObject.js
@@ -143,14 +143,25 @@ Ext.define('Workspace.objects.VectorObject', {
 			this.on('change:' + camel, _.bind(this.updateAttr, this, dash))
 		}, this);
 	},
+	getTransform: function() {
+		return "r" + ((+this.get('rotation') || 0));
+	},
 	updateAttr : function(attrName, value) {
-		// hack to prevent anti-aliasing for rectangular things
-		if(attrName=='x' || attrName=='y') {
-			value = Math.round(value)+0.5;
+		// positioning attributes get treated differently
+		if(attrName=='x' || attrName=='y' ||attrName=='width' || attrName=='height') {
+			
+			// hack to prevent anti-aliasing for rectangular things
+			if(attrName=='x' || attrName=='y') {
+				value = Math.round(value)+0.5;
+			}
+			this.vectorElement.transform("");
+			this.vectorElement.attr(attrName, value);
+			this.vectorElement.transform(this.getTransform());
+			return;
 		} else if (attrName=="rotation") {
-			// this.vectorElement.transform("");
-			// this.vectorElement.rotate(value);
-			//this.vectorElement.transform("r"+ (+value || 0));
+			this.vectorElement.transform("");
+			//this.vectorElement.rotate(value);
+			this.vectorElement.transform("r"+ (+value || 0));
 		}
 		this.vectorElement.attr(attrName, value);
 	},

static/client/workspace/objects/dna/Node.js
@@ -97,7 +97,7 @@ Ext.define('Workspace.objects.dna.Node', {
 	autoFill: false,
 	fill: '#ffffff',
 	stroke: '#000000',
-	theta: Math.PI,
+	theta: 180,
 	destroyChildren: true,
 	isResizable: false,
 

static/client/workspace/objects/dna/NodeLayout.js
@@ -6,7 +6,7 @@ Ext.define('Workspace.objects.dna.NodeLayout', {
 		if(!this.ignore) {
 			var count = this.getObjects().getCount(),
 			theta = this.idea.get('theta'),
-			dtheta = 2*Math.PI / count,
+			dtheta = 360 / count,
 			cx = this.idea.getCenterX(),
 			cy = this.idea.getCenterY(),
 			rx = this.idea.getRadiusX(),
@@ -17,11 +17,11 @@ Ext.define('Workspace.objects.dna.NodeLayout', {
 				// TODO: Make sure doLayout isn't applied before children are rendered
 				if(child.is('rendered')) {
 					child.setPosition(
-					parseInt(cx + Math.cos(theta) * rx - child.getWidth()) + this.paddingTop, // HACK
-					parseInt(cy + Math.sin(theta) * ry - child.getHeight()) + this.paddingLeft
+					parseInt(cx + Math.cos(Raphael.rad(theta)) * rx - child.getWidth()) + this.paddingTop, // HACK
+					parseInt(cy + Math.sin(Raphael.rad(theta)) * ry - child.getHeight()) + this.paddingLeft
 					);
-					child.set('rotation',Raphael.deg(theta)+(child.dtheta ? child.dtheta : 0 ));
-					theta = (theta + dtheta) % (2 * Math.PI);
+					child.set('rotation',theta+(child.dtheta ? child.dtheta : 0 ));
+					theta = (theta + dtheta) % (360);
 				}
 			},this);
 			this.ignore = false;
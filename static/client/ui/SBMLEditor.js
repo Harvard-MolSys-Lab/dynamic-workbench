c50e0ad34c0e430ae00144a45a22fa7023f6ee0e
Casey Grun <casey.grun@wyss.harvard.edu>
2012-06-15 15:23:44 -0400

    Small SBML updates

--- static/client/ui/SBMLEditor.js
+++ static/client/ui/SBMLEditor.js
@@ -13,24 +13,45 @@ Ext.define('App.ui.SBMLEditor', {
 		xtype: 'cite',
 		cite: 'sbml_2003'
 	}],
-
-	
+	url: 'http://sbml.org/validator/',
 	initComponent: function() {
-		this.mode = {
-				name: 'nupack',
-				multisubjective: true,
-			}
 		
 		this.extraTbarItems = (this.extraTbarItems || []); 
-		var tbar = this.extraTbarItems;
-		if(this.showSaveButton) {
-			tbar = tbar.concat(['->',Ext.create('App.ui.SaveButton',{
-				app: this,
-			})]);
-		}
+		var tbar = [{
+			text: 'Validate SBML',
+			href: this.url,
+			// handler: this.validateSBML,
+			// scope: this,
+			iconCls: 'tick',
+		},'->',{
+			text: 'SBML Reference',
+			iconCls: 'help',
+			href: 'http://sbml.org/Basic_Introduction_to_SBML',
+		},Ext.create('App.ui.SaveButton',{
+			app: this,
+		})];
 		Ext.apply(this, {
 			tbar: tbar
 		})
+		
+		this.form = Ext.create('Ext.form.Basic', this, {});
+		
 		this.callParent(arguments);
+	},
+	validateSBML: function() {
+		this.form.doAction('standardsubmit',{
+			params: this.getParams(),
+			target: '_blank',
+			url: this.url,
+			clientValidation: false,
+			method: 'post',
+			enctype:'multipart/form-data',
+		})
+	},
+	getParams: function() {
+		return {
+			'file':this.getValue(),
+			'output':'XHTML',
+		}
 	},
 })

9d3fdfa43a7398f0740fdec1d953363824f31566
Casey Grun <casey.grun@wyss.harvard.edu>
2012-06-27 13:56:58 -0400

    Update ConnectionLabel to use new Label interface, finally fixing the jumping +/- problem

--- static/client/workspace/ConnectionLabel.js
+++ static/client/workspace/ConnectionLabel.js
@@ -14,29 +14,8 @@ Ext.define('Workspace.ConnectionLabel', {
 			},
 			offsets: [0,0]
 		});
-		Workspace.ConnectionLabel.superclass.constructor.apply(this,arguments);
-	},
-	render: function() {
+		//Workspace.ConnectionLabel.superclass.constructor.apply(this,arguments);
 		this.callParent(arguments);
 	},
-	/**
-	 * onMove
-	 * Invoked when the element moves; repositions the shim
-	 * @private
-	 */
-	onMove : function() {
-		if(this.object) {
-			var oEl = this.object.getEl();
-			if(oEl) {
-				var el = this.getEl();
-				//el.alignTo(Ext.fly(oEl),this.position,this.offsets,this.animate);
-				el.position('absolute');
-				this.updateSize(this.object.get(this.property));
-				el.setLeftTop((this.object.getX() + this.object.getWidth() / 2 - el.getWidth() / 2)+this.offsets[0], (this.object.getY() + this.object.getHeight() / 2 - el.getHeight() / 2)+this.offsets[1]);
-			}
-		}
-	},
-	updateSize: function() {
-		this.callParent(arguments);
-	}
+	getLeft: function(el) {		return (this.object.getX() + this.object.getWidth() / 2 - el.getWidth() / 2)	},	getTop: function(el) {		return (this.object.getY() + this.object.getHeight() / 2 - el.getHeight() / 2)	},
 });

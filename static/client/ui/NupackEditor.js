421944d0c5786ade8e240ada7ff64ff62d422596
Casey Grun <casey.grun@wyss.harvard.edu>
2012-07-02 16:41:14 -0400

    Updates to NUPACK editor for sequence threading, convenience

--- static/client/ui/NupackEditor.js
+++ static/client/ui/NupackEditor.js
@@ -24,16 +24,18 @@ Ext.define('App.ui.NupackEditor', {
 	 */
 	showSaveButton:true,
 	
-	dockedItems: [{
-		xtype: 'cite',
-		cite: 'zadeh_etal_2011',
-	}],
-	
 	/**
 	 * @cfg
 	 * Enable additional multisubjective syntax
 	 */
 	multisubjective: false,
+	
+	/**
+	 * @cfg
+	 * Display citation bar
+	 */
+	showCite: true,
+	cite: 'zadeh_etal_2011',
 	
 	initComponent: function() {
 		if(this.multisubjective) {
@@ -76,8 +78,10 @@ Ext.define('App.ui.NupackEditor', {
 				 */
 				ref: 'editButton',
 				menu: [{
-					text: 'Thread segments to sequences',
-					handler: this.threadStrands
+					text: 'Thread segments to strands',
+					iconCls: 'thread-sequences',
+					handler: this.threadStrands,
+					scope: this,
 				},]
 			})
 		}
@@ -87,9 +91,48 @@ Ext.define('App.ui.NupackEditor', {
 			})]);
 		}
 		Ext.apply(this, {
-			tbar: tbar
-		})
+			tbar: tbar,
+			bbar: Ext.create('Ext.ux.statusbar.StatusBar',{
+				ref: 'statusBar',
+				items: [{
+					xtype: 'tbtext',
+					baseText: 'Lines: ',
+					text: 'Lines: ',
+					name: 'lineNo',
+				},'-',{
+					xtype: 'tbtext',
+					baseText: 'Cols: ',
+					text: 'Cols: ',
+					name: 'colNo',
+				}]
+			})
+		});
+		
+		if(this.showCite) {
+			Ext.apply(this,{
+				dockedItems: [{
+					xtype: 'cite',
+					cite: this.cite,
+				}],
+			});
+		}
+		
 		this.callParent(arguments);
+		this.selField = this.down('[name=selField]');
+		this.lineNo = this.down('[name=lineNo]');
+		this.colNo = this.down('[name=colNo]');
+		this.editor.on('cursorchange',this.updateStatusBar,this);
+	},
+	updateStatusBar: function() {
+		var sel = this.getSelection(),
+			selRange = this.getCursorRange();
+			
+		if(selRange[0].line != selRange[1].line || selRange[0].ch != selRange[1].ch) {
+			this.lineNo.setText('Lines: ' + [selRange[0].line,' &rarr; ',selRange[1].line,' (',Math.abs(selRange[1].line-selRange[0].line)+1,')'].join(''));
+			this.colNo.setText('Cols: ' + [selRange[0].ch,' &rarr; ',selRange[1].ch,' (',sel.length,')'].join(''));
+		} else {
+			this.lineNo.setText('Line: ' + selRange[0].line);			this.colNo.setText('Col:' + selRange[0].ch);
+		}
 	},
 	populateDesignWindow: function(menu,designWindow) {
 		designWindow.updateDesign(this.getValue());
@@ -102,7 +145,26 @@ Ext.define('App.ui.NupackEditor', {
 	threadStrands: function() {
 		var win = Ext.create('App.ui.SequenceThreader');
 		win.show();
-		win.setStrands(this.getSelection());
+		var seqs = _.chain(this.getValue().split('\n')).map(function(line) {
+			var parts = line.match(/domain\s+([\w\d]+)\s*[=:]\s*([A-Za-z]+)/);
+			if(parts != null && parts.length >= 3) {
+				return parts[1] + ' : ' + parts[2];
+			} else {
+				return null;
+			}
+		}).compact().value().join('\n');
+		
+		var strands = _.chain(this.getValue().split('\n')).map(function(line) {
+			var parts = line.match(/strand\s+([\w\d]+)\s*[=:]\s*(.+)/);
+			if(parts != null && parts.length >= 3) {
+				return parts[1] + ' : ' + parts[2];
+			} else {
+				return null;
+			}
+		}).compact().value().join('\n');
+				
+		win.setSequences(seqs);
+		win.setStrands(strands);
 	},
 }
 )

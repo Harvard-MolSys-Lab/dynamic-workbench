90b1d1936b629cd2cf94f276ad4583bed482a7e7
Casey Grun <casey.grun@wyss.harvard.edu>
2012-12-28 16:06:59 -0800

    Updated DD SequenceWindow to inherit from SequenceWindow

--- static/client/ui/DD/SequenceWindow.js
+++ static/client/ui/DD/SequenceWindow.js
@@ -1,40 +1,9 @@
 Ext.define('App.ui.dd.SequenceWindow',{
-	extend: 'Ext.window.Window',
-	width: 800,
-	height: 600,
-	layout: 'fit',
+	extend: 'App.ui.SequenceWindow',
 	title: 'Add specific sequences to DD',
-	closeAction: 'hide',
 	buttonText: 'Add Domains',
-	buttonIconCls: 'tick',
 	value: '',
-	handler: function(value) {
+	handler: function(domains,value) {
 		this.designer.addDomains(value.split('\n'));
 	},
-	initComponent: function() {
-		this.sequenceEditor = Ext.create('App.ui.CodeMirror',{mode: 'sequence',border: false});
-		Ext.apply(this,{
-			items: [this.sequenceEditor],
-			buttons: [{
-				text: this.buttonText,
-				iconCls: this.buttonIconCls,
-				handler: this.addDomains,
-				scope: this,
-			}]
-		});
-		this.callParent(arguments);
-		this.sequenceEditor.setValue(this.value);
-	},
-	addDomains: function() {
-		var data = this.getValue();
-		if(this.scope) { this.handler.call(this.scope,data); }
-		else { this.handler(data); }
-		this.close();
-	},
-	setValue: function(value) {
-		this.sequenceEditor.setValue(value);
-	},
-	getValue: function() {
-		return this.sequenceEditor.getValue();
-	}
 })

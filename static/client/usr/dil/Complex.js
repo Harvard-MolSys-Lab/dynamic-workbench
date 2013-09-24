fd395b9682e7bef4933ac6d98b8a6bcd00b30a5c
Casey Grun <casey.grun@wyss.harvard.edu>
2013-09-24 18:40:06 -0400

    Pulled DIL model classes into different files for better dependency management

---
+++ static/client/usr/dil/Complex.js
@@ -0,0 +1,58 @@
+/**
+ * @class
+ * Record defining a complex. Use with App.usr.dil.ComplexStore
+ */
+Ext.define('App.usr.dil.Complex', {
+	extend: 'Ext.data.Model',
+	requires: ['App.usr.dil.StrandStore','App.usr.dil.SegmentStore'],
+	fields: [{
+		name: 'id',
+		type: 'int'
+	}, {
+		name: 'name'
+	}, {
+		name: 'polarity',
+		type: 'int'
+	}, {
+		name: 'strands' // array
+	}, {
+		name: 'specs',
+		// array
+	}, {
+		name: 'sequences',
+		// array
+	}, {
+		name: 'structure'
+	}, ],
+	idgen: 'sequential',
+	getDynaml: function(lib) {
+		return lib.getNode(this.get('name'));
+	},
+	getStrands: function() {
+		return this.get('strands');
+	},
+	getStructures: function() {
+		var structures = this.get('structure') || '';
+		return  _.map(structures.split('+'), function(s) {
+			return s.trim();
+		});
+	},
+	fixStructure: function(strandIndex,added,removed) {
+		var structures = this.getStructures(),
+			struct = structures[strandIndex];
+		if(struct) {
+			struct = struct.split('');
+			for(var i=struct.length-1; i>=0; i--) {
+				if(removed.indexOf(i) != -1) {
+					delete struct[i];
+				}
+				if(added.indexOf(i) != -1) {
+					struct.splice(i,0,'.');
+				}
+			}
+			structures[strandIndex] = _.compact(struct).join('');
+			this.set('structure',structures.join('+'));
+		}
+	},
+	proxy: 'memory',
+});

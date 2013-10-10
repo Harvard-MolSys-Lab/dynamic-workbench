d7378a6fa0b97c4461a6e7bbe159c130b104542a
Casey Grun <casey.grun@wyss.harvard.edu>
2013-10-10 18:33:47 -0400

    Factored out DIL editor highlighting into separate class

--- static/client/usr/dil/DilEditor.js
+++ static/client/usr/dil/DilEditor.js
@@ -265,33 +265,22 @@ Ext.define('App.usr.dil.DilEditor', {
 		this.complexView = this.down('[name=complexView]');
 		this.down('[name=complexViewMenu]').view = this.complexView;
 
-
 		this.segmentStore.on('update', function(store, rec, operation, modifiedFieldNames) {
 			this.updateStrandSequences();
 		}, this);
+		
+		this.highlightManager = Ext.create('App.usr.dil.HighlightManager',{
+			segmentStore: this.segmentStore,
+			strandStore: this.strandStore,
+			complexStore: this.complexStore,
 
-		// Highlight items in the #strandsGrid and #complexView when they're moused over in the segments grid
-		this.segmentsGrid.on('itemmouseenter', function(grid, rec, el, e) {
-			this.fireEvent('updateSegmentHighlight', rec.get('identity'), 1);
-		}, this);
-		this.segmentsGrid.on('itemmouseleave', function(grid, rec, el, e) {
-			this.fireEvent('updateSegmentHighlight', null);
-		}, this);
-		this.on('updateSegmentHighlight', this.updateSegmentHighlight, this, {
-			buffer: 10,
+			segmentsGrid: this.segmentsGrid,
+			strandsGrid: this.strandsGrid,
+			complexView: this.complexView
 		});
-
-		// Highlight items in #segmentsGrid and #complexView when they're moused over in the strands grid
 		this.on('afterrender', function() {
-			this.strandsGrid.getEl().on('mouseover', function(e, el) {
-				var identity = el.getAttribute('data-segment-identity'),
-					polarity = el.getAttribute('data-segment-polarity');
-				this.fireEvent('updateSegmentHighlight', identity, polarity);
-			}, this, {
-				delegate: 'span.sequence-segment'
-			});
-		}, this);
-
+			this.highlightManager.afterrender();
+		},this);
 
 	},
 	onLoad: function() {
@@ -419,48 +408,6 @@ Ext.define('App.usr.dil.DilEditor', {
 		if(node) {
 			this.editComplexWindows[name].alignTo(node, 'tl-tl');
 		}
-	},
-
-	/* ------------------------------------------------------------------------------------------- 
-	   Visualization                                                                             */
-
-	updateSegmentHighlight: function(identity, polarity) {
-		if(identity) {
-			this.highlightSegment(identity, polarity);
-		} else {
-			this.unhighlightSegment();
-		}
-	},
-	/**
-	 * Highlights the passed segment with the given polarity in the #complexView and #strandsGrid
-	 * @param  {String} segment Segment identity
-	 * @param  {Number} polarity Segment polarity (-1 or 1)
-	 */
-	highlightSegment: function(segment, polarity) {
-		this.complexView.preview.fade();
-		this.complexView.preview.highlight({
-			'segment_identity': segment,
-			'segment_polarity': polarity
-		}, 'node-highlight');
-		this.complexView.preview.highlight({
-			'segment_identity': segment,
-			'segment_polarity': -1 * polarity
-		}, 'node-highlight-complement');
-
-		this.strandsGrid.unhighlightSegment();
-		this.strandsGrid.highlightSegment(segment, polarity);
-	},
-	/**
-	 * Unhighlights the passed segment with the given polarity in the #complexView and #strandsGrid
-	 * @param  {String} segment Segment identity
-	 * @param  {Number} polarity Segment polarity (-1 or 1)
-	 */
-	unhighlightSegment: function(segment, polarity) {
-		this.complexView.preview.unfade();
-		this.complexView.preview.unhighlight(null, 'node-highlight');
-		this.complexView.preview.unhighlight(null, 'node-highlight-complement');
-
-		this.strandsGrid.unhighlightSegment();
 	},
 
 	/* ------------------------------------------------------------------------------------------- 

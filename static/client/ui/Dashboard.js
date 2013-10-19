d994a6779bac135f5f0696b6034c8606c1b94103
Casey Grun <casey.grun@wyss.harvard.edu>
2013-10-18 21:18:43 -0400

    Updated Dashboard to use better icons

--- static/client/ui/Dashboard.js
+++ static/client/ui/Dashboard.js
@@ -153,7 +153,7 @@ Ext.define('App.ui.Dashboard', {
 		        	}]
 		        },{
 		        	text: 'Enumerate',
-		        	iconCls: 'enumerate-24',
+		        	iconCls: 'enumerate-secondary-24',
 		        	handler: App.ui.Launcher.makeLauncher('enumedit'),
 		        	disabled: true,
 		        },{

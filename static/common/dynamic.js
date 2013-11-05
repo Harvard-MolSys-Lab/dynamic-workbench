Harvard-MolSys-Lab/dynamic-workbench

commit b4aeb2eb4605701b9d00484cdbde6c9b57f6b97f

Changed NUPACK, MS output convention to not append _structure to structure names

static/common/dynamic.js
@@ -2049,7 +2049,7 @@ App.dynamic = module.exports = (function(_,DNA) {
 				return 'd'+id;
 			}
 			function structureName(nodeName) {
-				return nodeName + '_structure';
+				return nodeName; // + '_structure';
 			}
 
 			
@@ -2060,7 +2060,11 @@ App.dynamic = module.exports = (function(_,DNA) {
 			// 		sodium[M] = 1.0       # optional units: M (default), mM, uM, nM, pM
 			// 		dangles = some
 			
-			out = out.concat(["material = dna", "temperature[C] = 23.0", "trials = 3", "sodium[M] = 1.0", "dangles = some"]);
+			out = out.push(["material = dna", 
+				"temperature[C] = 23.0", 
+				"trials = 3", 
+				"sodium[M] = 1.0", 
+				"dangles = some"].join('\n'));
 
 
 			// print multisubjective-specific metadata and structure info

commit 7a6fed87757926135ce636f76de7d9490b6c3250

Hotfix to accidental regression introduced in b4aeb2e

static/common/dynamic.js
@@ -2060,7 +2060,7 @@ App.dynamic = module.exports = (function(_,DNA) {
 			// 		sodium[M] = 1.0       # optional units: M (default), mM, uM, nM, pM
 			// 		dangles = some
 			
-			out = out.push(["material = dna", 
+			out.push(["material = dna", 
 				"temperature[C] = 23.0", 
 				"trials = 3", 
 				"sodium[M] = 1.0", 
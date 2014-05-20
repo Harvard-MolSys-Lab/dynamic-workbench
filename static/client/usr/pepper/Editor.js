69e534b1efbf1bca85a55932002c4655120ffd50
Casey Grun <casey.grun@wyss.harvard.edu>
2014-05-20 09:34:34 -0400

    Updated pepper to accept arguments

--- static/client/usr/pepper/Editor.js
+++ static/client/usr/pepper/Editor.js
@@ -5,25 +5,42 @@ Ext.define('App.usr.pepper.Editor', {
 	extend: 'App.usr.text.Editor',
 	title: 'Pepper',
 	initComponent: function() {
+		this.argsField = Ext.create('Ext.form.field.Text',{
+			indent: true,
+			allowBlank:true
+		});
 		Ext.applyIf(this, {
 			tbar: [{
-				text: 'Save',
-				iconCls: 'save',
-				handler: function() {
-					this.saveFile();
-				},
-				scope: this,
-			},{
 				text: 'Compile',
 				iconCls: 'compile',
-				handler: function() {
-					App.runTask('Pepper', {
-						node: this.getDocumentPath()
-					});
-				},
+				xtype: 'splitbutton',
+				menu: [{
+					text: 'Arguments:',
+					canActivate: false,
+				},this.argsField,{
+					text: 'Compile',
+					iconCls: 'compile',
+					handler: this.runPepper,
+					scope: this
+				}],
+				handler: this.runPepper,
 				scope: this,
-			}]
+			},'->',{
+				text: 'Help',
+				iconCls: 'help',
+				handler: App.ui.Launcher.makeLauncher('help:pepper'),
+			}, Ext.create('App.ui.SaveButton',{
+				text: 'Save',
+				iconCls: 'save',
+				app: this,
+			})]
 		});
 		this.callParent(arguments);
+	},
+	runPepper: function() {
+		App.runTask('Pepper', {
+			node: this.getDocumentPath(),
+			args: this.argsField.getValue()
+		});
 	}
 });
--- static/client/usr/pepper/PilEditor.js
+++ static/client/usr/pepper/PilEditor.js
@@ -3,13 +3,13 @@
  */
 Ext.define('App.usr.pepper.PilEditor', {
 	extend: 'App.usr.text.Editor',
-	requires: ['App.usr.enum.RunButton'],
+	requires: ['App.usr.enum.RunButton', 'App.ui.ConvertButton'],
 	title: 'PIL',
 	initComponent: function() {
 		Ext.applyIf(this, {
 			tbar: [
 			Ext.create('App.usr.enum.RunButton',{app: this}),
-
+			Ext.create('App.ui.ConvertButton',{app:this, fromMode: 'pil'}),
 			 '->', Ext.create('App.ui.SaveButton', {
 				app: this
 			})]
